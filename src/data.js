8dc0ba612c300e96097f24fcd2911ce00729e1dc
Aaqibhafeez Khan <aaqibhafeezkhan@gmail.com>
2025-02-03 01:29:38 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,22 @@
-
+export const questions = [
+    {
+        question: "Do you have a fever?",
+        weight: 0.2
+    },
+    {
+        question: "Are you experiencing shortness of breath?",
+        weight: 0.3
+    },
+    {
+        question: "Do you have a persistent cough?",
+        weight: 0.25
+    },
+    {
+        question: "Are you feeling fatigued?",
+        weight: 0.15
+    },
+    {
+        question: "Have you lost your sense of taste/smell?",
+        weight: 0.1
+    }
+];
